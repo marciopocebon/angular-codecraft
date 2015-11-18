53fff60227e8a9df8363b8329ac1d7c950167e98
willianjusten <willianjustenqui@gmail.com>
2015-11-18 02:19:19 -0200

    Add angular core validation

--- section1/app/main.js
+++ section1/app/main.js
@@ -3,16 +3,22 @@ var app = angular.module('minmax', []);
 app.controller('MinMaxCtrl', function($scope, $http){
     $scope.formModel = {};
 
-    $scope.onSubmit = function() {
-        console.log('Hey Im submitted');
-        console.log($scope.formModel);
+    $scope.onSubmit = function(valid) {
 
-        $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
-            .success(function(data){
-                console.log('=)')
-            })
-            .error(function(data){
-                console.log('=(')
-            });
+        if (valid) {
+            console.log('Hey Im submitted');
+            console.log($scope.formModel);
+
+            $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+                .success(function(data){
+                    console.log('=)')
+                })
+                .error(function(data){
+                    console.log('=(')
+                });
+        }
+        else {
+            console.log('Invalid Form!');
+        }
     }
 });
