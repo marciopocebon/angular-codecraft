3eadd083f22854b2610ed47d1f9a68d40b88dbbc
willianjusten <willianjustenqui@gmail.com>
2015-11-20 15:00:49 -0200

    Add ordering via api

--- connect-rest-api/app/main.js
+++ connect-rest-api/app/main.js
@@ -1,83 +1,102 @@
 var app = angular.module('codecraft', [
-	'ngResource',
-	'infinite-scroll'
+    'ngResource',
+    'infinite-scroll'
 ]);
 
 app.config(function($httpProvider, $resourceProvider) {
-	$httpProvider.defaults.headers.common['Authorization'] = 'Token 7a37c6d96f31d21ce50d64d37f16cd5cdb9c160c';
-	$resourceProvider.defaults.stripTrailingSlashes = false;
+    $httpProvider.defaults.headers.common['Authorization'] = 'Token 7a37c6d96f31d21ce50d64d37f16cd5cdb9c160c';
+    $resourceProvider.defaults.stripTrailingSlashes = false;
 });
 
 app.factory('Contact', function($resource) {
-	return $resource('http://codecraftpro.com/api/samples/v1/contact/:id/');
+    return $resource('http://codecraftpro.com/api/samples/v1/contact/:id/');
 });
 
 app.controller('PersonDetailController', function ($scope, ContactService) {
-	$scope.contacts = ContactService;
+    $scope.contacts = ContactService;
 });
 
 app.controller('PersonListController', function ($scope, ContactService) {
 
-	$scope.search = "";
-	$scope.order = "email";
-	$scope.contacts = ContactService;
+    $scope.search = "";
+    $scope.order = "email";
+    $scope.contacts = ContactService;
 
-	$scope.loadMore = function () {
-		$scope.contacts.loadMore();
-	};
+    $scope.loadMore = function () {
+        $scope.contacts.loadMore();
+    };
 
-	$scope.sensitiveSearch = function (person) {
-		if ($scope.search) {
-			return person.name.indexOf($scope.search) == 0 ||
-				person.email.indexOf($scope.search) == 0;
-		}
-		return true;
-	};
+    $scope.$watch('search', function (newVal, oldVal) {
+        if (angular.isDefined(newVal)){
+            $scope.contacts.doSearch(newVal);
+        }
+    });
 
+    $scope.$watch('order', function (newVal, oldVal) {
+        if (angular.isDefined(newVal)){
+            $scope.contacts.doOrder(newVal);
+        }
+    });
 });
 
 app.service('ContactService', function (Contact) {
 
-	var self =  {
-		'addPerson': function (person) {
-			this.persons.push(person);
-		},
-		'page': 1,
-		'hasMore': true,
-		'isLoading': false,
-		'selectedPerson': null,
-		'persons': [],
-		'loadContacts': function () {
-			if (self.hasMore && !self.isLoading) {
-				self.isLoading = true;
+    var self =  {
+        'addPerson': function (person) {
+            this.persons.push(person);
+        },
+        'page': 1,
+        'hasMore': true,
+        'isLoading': false,
+        'selectedPerson': null,
+        'persons': [],
+        'doSearch': function (search) {
+            self.hasMore = true;
+            self.page = 1;
+            self.persons = [];
+            self.search = search;
+            self.loadContacts();
+        },
+        'doOrder': function (order) {
+            self.hasMore = true;
+            self.page = 1;
+            self.persons = [];
+            self.ordering = order;
+            self.loadContacts();
+        },
+        'loadContacts': function () {
+            if (self.hasMore && !self.isLoading) {
+                self.isLoading = true;
 
-				var params = {
-					page: self.page
-				};
+                var params = {
+                    page: self.page,
+                    search: self.search,
+                    ordering: self.ordering
+                };
 
-				Contact.get(params, function (data) {
-					angular.forEach(data.results, function(person) {
-						self.persons.push(new Contact(person));
-					})
+                Contact.get(params, function (data) {
+                    angular.forEach(data.results, function(person) {
+                        self.persons.push(new Contact(person));
+                    })
 
-					if (!data.next) {
-						self.hasMore = false;
-					}
+                    if (!data.next) {
+                        self.hasMore = false;
+                    }
 
-					self.isLoading = false;
-				});
-			}
-		},
-		'loadMore': function () {
-			if (self.hasMore && !self.isLoading) {
-				self.page += 1;
-				self.loadContacts();
-			}
-		}
-	};
+                    self.isLoading = false;
+                });
+            }
+        },
+        'loadMore': function () {
+            if (self.hasMore && !self.isLoading) {
+                self.page += 1;
+                self.loadContacts();
+            }
+        }
+    };
 
-	self.loadContacts();
-	return self;
+    self.loadContacts();
+    return self;
 });
 
 
