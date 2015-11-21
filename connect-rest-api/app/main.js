23330ae4200cb8083857be9fcbcdbf03a53da8f2
willianjusten <willianjustenqui@gmail.com>
2015-11-21 21:14:49 -0200

    Create different views and manipulate using ui-router

--- connect-rest-api/app/main.js
+++ connect-rest-api/app/main.js
@@ -14,14 +14,35 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('list', {
             url: '/',
-            templateUrl: 'templates/list.html',
-            controller: 'PersonListController'
+            views: {
+                main: {
+                    templateUrl: 'templates/list.html',
+                    controller: 'PersonListController'
+                },
+                search: {
+                    templateUrl: 'templates/searchform.html',
+                    controller: 'PersonListController'
+                }
+            }
         })
         .state('edit', {
             url: '/edit/:email',
-            templateUrl: 'templates/edit.html',
-            controller: 'PersonDetailController'
+            views: {
+                main: {
+                    templateUrl: 'templates/edit.html',
+                    controller: 'PersonDetailController'
+                }
+            }
         })
+        .state('create', {
+            url: '/create/',
+            views: {
+                main: {
+                    templateUrl: 'templates/edit.html',
+                    controller: 'PersonCreateController'
+                }
+            }
+        });
 
     $urlRouterProvider.otherwise('/');
 });
@@ -55,19 +76,39 @@ app.filter("defaultImage", function (){
     }
 });
 
-app.controller('PersonDetailController', function ($scope, $stateParams, ContactService) {
+app.controller('PersonDetailController', function ($scope, $stateParams, $state, ContactService) {
+
     $scope.contacts = ContactService;
+    $scope.contacts.selectedPerson = $scope.contacts.getPerson($stateParams.email);
+    $scope.mode = 'Edit';
 
     $scope.save = function () {
-        $scope.contacts.updateContact($scope.contacts.selectedPerson);
+        $scope.contacts.updateContact($scope.contacts.selectedPerson).then(function(){
+            $state.go('list');
+        });
     }
 
     $scope.remove = function () {
-        $scope.contacts.removeContact($scope.contacts.selectedPerson);
+        $scope.contacts.removeContact($scope.contacts.selectedPerson).then(function(){
+            $state.go('list');
+        });
     }
 });
 
-app.controller('PersonListController', function ($scope, $modal, ContactService) {
+app.controller('PersonCreateController', function ($scope, $state, ContactService) {
+
+    $scope.contacts = ContactService;
+    $scope.mode = 'Create';
+
+    $scope.save = function () {
+        $scope.contacts.createContact($scope.contacts.selectedPerson).then(function(){
+            $state.go('list');
+        });
+    }
+
+});
+
+app.controller('PersonListController', function ($scope, ContactService) {
 
     $scope.search = "";
     $scope.order = "email";
@@ -76,61 +117,36 @@ app.controller('PersonListController', function ($scope, $modal, ContactService)
     $scope.loadMore = function () {
         $scope.contacts.loadMore();
     };
-
-    $scope.showCreateModal = function () {
-        $scope.contacts.selectedPerson = {};
-        $scope.createModal = $modal({
-            scope: $scope,
-            template: 'templates/modal.create.tpl.html',
-            show: true
-        });
-    };
-
-    $scope.createContact = function () {
-        $scope.contacts.createContact($scope.contacts.selectedPerson)
-            .then(function(){
-                $scope.createModal.hide();
-            });
-    }
-
-    $scope.$watch('search', function (newVal, oldVal) {
-        if (angular.isDefined(newVal)){
-            $scope.contacts.doSearch(newVal);
-        }
-    });
-
-    $scope.$watch('order', function (newVal, oldVal) {
-        if (angular.isDefined(newVal)){
-            $scope.contacts.doOrder(newVal);
-        }
-    });
 });
 
-app.service('ContactService', function (Contact, $q, toaster) {
+app.service('ContactService', function (Contact, $q, $rootScope, toaster) {
 
     var self =  {
-        'addPerson': function (person) {
-            this.persons.push(person);
-        },
         'page': 1,
         'hasMore': true,
         'isLoading': false,
         'isSaving': false,
         'selectedPerson': null,
-        'ordering': 'email',
+        'ordering': 'name',
         'persons': [],
-        'doSearch': function (search) {
+        'getPerson': function (email) {
+            for (var i = 0; i <= self.persons.length; i++){
+                var person = self.persons[i];
+                if (email == person.email){
+                    return person;
+                }
+            }
+        },
+        'doSearch': function () {
             self.hasMore = true;
             self.page = 1;
             self.persons = [];
-            self.search = search;
             self.loadContacts();
         },
-        'doOrder': function (order) {
+        'doOrder': function () {
             self.hasMore = true;
             self.page = 1;
             self.persons = [];
-            self.ordering = order;
             self.loadContacts();
         },
         'loadContacts': function () {
@@ -163,13 +179,17 @@ app.service('ContactService', function (Contact, $q, toaster) {
             }
         },
         'updateContact': function (person) {
+            d = $q.defer();
             self.isSaving = true
             person.$update().then(function() {
                 self.isSaving = false;
                 toaster.pop('success', 'Updated ' + person.name);
+                d.resolve();
             });
+            return d.promise
         },
         'removeContact': function (person) {
+            d = $q.defer();
             self.isDeleting = true
             person.$remove().then(function() {
                 self.isDeleting = false;
@@ -177,7 +197,9 @@ app.service('ContactService', function (Contact, $q, toaster) {
                 self.persons.splice(index,1);
                 self.selectedPerson = null;
                 toaster.pop('success', 'Deleted ' + person.name);
+                d.resolve();
             });
+            return d.promise;
         },
         'createContact': function (person) {
             d = $q.defer();
@@ -193,10 +215,29 @@ app.service('ContactService', function (Contact, $q, toaster) {
             });
 
             return d.promise;
+        },
+        'watchFilters': function () {
+            $rootScope.$watch(function () {
+                return self.search;
+            }, function (newVal) {
+                if (angular.isDefined(newVal)) {
+                    self.doSearch();
+                }
+            });
+
+            $rootScope.$watch(function () {
+                return self.ordering;
+            }, function (newVal) {
+                if (angular.isDefined(newVal)) {
+                    self.doOrder();
+                }
+            });
         }
     };
 
     self.loadContacts();
+    self.watchFilters();
+
     return self;
 });
 
