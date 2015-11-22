3cdfbbffddf58b62d95551f3425aceb6fc96dfd0
willianjusten <willianjustenqui@gmail.com>
2015-11-22 00:21:45 -0200

    Add card directive

--- connect-rest-api/app/main.js
+++ connect-rest-api/app/main.js
@@ -77,6 +77,25 @@ app.directive('ccSpinner', function (){
     }
 });
 
+app.directive('ccCard', function (){
+    return  {
+        restrict: 'AE',
+        templateUrl: 'templates/card.html',
+        scope: {
+            'user': '='
+        },
+        controller: function ($scope, ContactService) {
+            $scope.isDeleting = false;
+            $scope.deleteUser = function () {
+                $scope.isDeleting = true;
+                ContactService.removeContact($scope.user).then(function(){
+                    $scope.isDeleting = false;
+                });
+            }
+        }
+    }
+});
+
 app.filter("defaultImage", function (){
     return function(input, param) {
         if(!input) {
